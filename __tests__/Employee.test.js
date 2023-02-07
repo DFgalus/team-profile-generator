b9fbf9f0c077cd8380a3057dfb3f08acfb140d96
Dgalus <damiangalus7@gmail.com>
2023-02-07 17:27:50 -0500

    Started writing test for Employee class, and created all constructors for classes

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('../lib/Employee')
+
+describe('Employee class', () => {
+    it('Creates a new Employee object'), () => {
+        const employee = new Employee()
+    }
+})

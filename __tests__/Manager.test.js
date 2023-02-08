89255e4f5690264825a61912ea50abac432404e8
Dgalus <damiangalus7@gmail.com>
2023-02-08 18:16:58 -0500

    Added officeNumber to manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,7 +3,7 @@ const Manager = require('../lib/Manager');
 
 describe('Manager class', () => {
     it('Creates a new Manager object'), () => {
-        const manager = new Manager("Bob", 15, "BobRoss@gmail.com");
+        const manager = new Manager("Bob", 15, "BobRoss@gmail.com", 6);
         expect(manager.officeNumber).toEqual(expect.any(Number));
     }
 });
@@ -12,7 +12,7 @@ describe('Manager class', () => {
 describe('Manager class', () => {
     describe('getRole', () => {
         it('should return employee role', () => {
-            const manager = new Manager("Bob", 15, "BobRoss@gmail.com");
+            const manager = new Manager("Bob", 15, "BobRoss@gmail.com", 6);
             expect(manager.getRole()).toEqual("Manager");
         });
     });
